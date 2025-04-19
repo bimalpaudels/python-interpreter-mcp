e83c101b211b5650a72dc27af75c5e636494f13f
Bimal Paudel <ibimalp@gmail.com>
2025-04-19 11:23:22 +0200

    Feat: Inital mcp server setup

--- src/python_interpreter_mcp/__init__.py
+++ src/python_interpreter_mcp/__init__.py
@@ -1,2 +1,13 @@
-def main() -> None:
-    print("Hello from python-interpreter-mcp!")
+from .server import server
+
+
+def main():
+    """Main entrypoint for the MCP server."""
+    import asyncio
+
+    print("Starting Python-Interpreter-MCP server")
+    asyncio.run(server.run())
+
+
+if __name__ == "__main__":
+    main()
